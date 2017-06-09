408ae2517ebcaf91c646de968dddac63de789342
Zhou <jessezhou@deloitte.com.au>
2017-06-09 23:25:19 +1000

    edited formatting

--- _posts/2017-06-05-google-optimise-experiment.js
+++ _posts/2017-06-05-google-optimise-experiment.js
@@ -7,8 +7,10 @@ description: A/B test in progress...
 Time to test what Google Optimise can do!
 
 I have just created a test Google Optimise Multivariate page with 2 different variations.
+Google Optimise just release this FREE A/B testing tool that is part of the Google 360 suite.
+I've deployed some Simple Google Optimise code across this site, and have also integrated this with Google Analytics for reporting purposes.
 
-Google Optimise just release this FREE A/B testing tool that is part of the Google 360 suite. I've deployed some Simple Google Optimise code across this site, and have also integrated this with Google Analytics for reporting purposes.
+
 
 With the free version, you are able to perform:
 - A/B testing
@@ -17,8 +19,12 @@ With the free version, you are able to perform:
 - Redirect tests
 - Some targeting
 
+
+
 The full version (Optimize 360) will provide:
 - Google Analytics audience targeting
+
+
 
 As part of an A/B test you can:
 - edit an experiment with the Visual Editor
